390078c77542f1d94a8cae2d3361c323ad95b99d
Yaroslav Vorobyov <official.vorobyov.yaroslav@gmail.com>
2023-03-20 12:33:22 +0200

    Added attributes for enums and fields

--- ManagedCode.OpenAI/Chats/Enums/ChatModel.cs
+++ ManagedCode.OpenAI/Chats/Enums/ChatModel.cs
@@ -1,14 +1,17 @@
 using System.ComponentModel;
+using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 
 namespace ManagedCode.OpenAI.Chats.Enums;
 
-[JsonConverter(typeof(JsonStringEnumConverter))]
+[JsonConverter(typeof(JsonStringEnumMemberConverter))]
 public enum ChatModel
 {
+    [EnumMember(Value = "gpt-3.5-turbo")]
     [Description("gpt-3.5-turbo")]
     Turbo,
     
+    [EnumMember(Value = "gpt-3.5-turbo-0301")]
     [Description("gpt-3.5-turbo-0301")]
     Turbo0301
 }
