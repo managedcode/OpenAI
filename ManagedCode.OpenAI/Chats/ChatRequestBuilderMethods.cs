f09e9e08287d6113dafb859c8b4c1987e8d80af9
Yaroslav Vorobyov <official.vorobyov.yaroslav@gmail.com>
2023-03-20 12:37:48 +0200

    Removed methods and renamed: 'AsChat' -> 'CreateChatBuilder'

--- ManagedCode.OpenAI/Chats/ChatRequestBuilderMethods.cs
+++ ManagedCode.OpenAI/Chats/ChatRequestBuilderMethods.cs
@@ -1,24 +1,16 @@
 using ManagedCode.OpenAI.Chats.Enums;
+using ManagedCode.OpenAI.Client;
+using ManagedCode.OpenAI.Methods;
 using ManagedCode.OpenAI.Models;
 
 namespace ManagedCode.OpenAI.Chats;
 
 public static class ChatRequestBuilderMethods
 {
-    public static ChatRequestBuilder AsChat(this OpenAIClient.OpenAIClient client, string modelId)
+    
+    public static ChatRequestBuilder CreateChatBuilder(this OpenAIClient client, ChatModel model)
     {
-        return new ChatRequestBuilder(client, modelId);
+        return new ChatRequestBuilder(client, model);
     }
-    
-    public static ChatRequestBuilder AsChat(this OpenAIClient.OpenAIClient client, Model model)
-    {
-        return AsChat(client, model.Id);
-    }
-    
-    public static ChatRequestBuilder AsChat(this OpenAIClient.OpenAIClient client, ChatModel model)
-    {
-        return AsChat(client, model);
-    }
-    
-    
+
 }
