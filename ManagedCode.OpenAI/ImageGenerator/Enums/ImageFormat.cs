3611aa78969ed3de9b4ec0108a8b0d747243c729
Yaroslav Vorobyov <yarikhelov@gmail.com>
2023-03-21 09:22:50 +0200

    Added attributes for enums ImageResolution and ImageResult

--- ManagedCode.OpenAI/ImageGenerator/Enums/ImageFormat.cs
+++ ManagedCode.OpenAI/ImageGenerator/Enums/ImageFormat.cs
@@ -1,12 +1,18 @@
 using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using ManagedCode.OpenAI.Chats.Enums;
 
 namespace ManagedCode.OpenAI.ImageGenerator.Enums;
 
+[JsonConverter(typeof(JsonStringEnumMemberConverter))]
 public enum ImageFormat
 {
+    [EnumMember(Value = "url")]
     [Description("url")]
     Url,
     
+    [EnumMember(Value = "b64_json")]
     [Description("b64_json")]
     Base64Json,
 }
