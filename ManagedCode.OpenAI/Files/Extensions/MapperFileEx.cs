8d8e4f761177eeae992de4ecec8517b59e1945ea
Yaroslav Vorobyov <yarikhelov@gmail.com>
2023-03-27 16:27:11 +0300

    Added Mapper for FileManager

---
+++ ManagedCode.OpenAI/Files/Extensions/MapperFileEx.cs
@@ -0,0 +1,32 @@
+using ManagedCode.OpenAI.Files.Abstractions;
+using ManagedCode.OpenAI.Files.Models;
+
+namespace ManagedCode.OpenAI.Files.Extensions;
+
+internal static class MapperFileEx
+{
+    public static IFileInfo ToFileInfo(this FileInfoDto dto)
+    {
+        return new FileInfo()
+        {
+            Bytes = dto.Bytes,
+            CreatedAt = DateTimeOffset.FromUnixTimeSeconds(dto.CreatedAt).DateTime,
+            Filename = dto.Filename,
+            Id = dto.Id,
+            Purpose = dto.Purpose
+        };
+    }
+    
+    public static IFileInfo[] ToFileInfoArray(this IDataArrayResponseDto<FileInfoDto> dto)
+    {
+        return dto.Data
+            .Select(e => e.ToFileInfo())
+            .ToArray();
+    }
+    
+    public static bool GetDeleteResult(this FileDeleteResponseDto dto)
+    {
+        return dto.Deleted;
+    }
+    
+}
