1424c95b3f11c492cb9e93d7711030eb5b7d8156
Alexandr Ivanitskyi <6saaanchik6@gmail.com>
2023-07-19 13:16:05 +0300

    Add Azure OpenAI

---
+++ ManagedCode.OpenAI.Tests/AzureOpenAIChatTests.cs
@@ -0,0 +1,45 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using FluentAssertions;
+using ManagedCode.OpenAI.Tests.Attributes;
+using ManagedCode.OpenAI.Tests.Base;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ManagedCode.OpenAI.Tests;
+
+public class AzureOpenAIChatTests : BaseTestClass
+{
+    public AzureOpenAIChatTests(ITestOutputHelper output, IConfiguration configuration) 
+        : base(output, configuration)
+    {
+    }
+    
+    [Fact]
+    public async Task AskSingle_Success()
+    {
+        var uri = new Uri("");
+        var cred = new AzureKeyCredential("");
+
+        var options = new ChatCompletionsOptions
+        {
+            MaxTokens = 400,
+            Temperature = 1f,
+            FrequencyPenalty = 0.0f,
+            PresencePenalty = 0.0f,
+            NucleusSamplingFactor = 0.95f // Top P
+        };
+        
+        var chat = AzureClientBuilder.InitializateClient(uri, cred)
+            .Configure(options)
+            .Build();
+
+        var response = await chat.AskAsync("How are you?");
+        var response2 = await chat.AskAsync("Tell me what is C#");
+        var response3 = await chat.AskAsync("Where we can use them?");
+        var response4 = await chat.AskAsync("Help me describe creating modern desktop app with c#");
+
+        response.Should().NotBeNull();
+    }
+}
---
+++ ManagedCode.OpenAI/Chat/AzureOpenAIChat.cs
@@ -0,0 +1,50 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using ManagedCode.OpenAI.Client;
+
+namespace ManagedCode.OpenAI.Chat;
+
+public class AzureOpenAIChat : IAzureOpenAIChat
+{
+    private readonly ChatRole ChatRole = ChatRole.System;
+    
+    private OpenAIClient _client;
+    private ChatCompletionsOptions _options;
+
+    public AzureOpenAIChat(OpenAIClient client, ChatCompletionsOptions options)
+    {
+        _client = client;
+        _options = options;
+    }
+    
+    public async Task<string> AskAsync(string message)
+    {
+        _options.Messages.Add(new Azure.AI.OpenAI.ChatMessage(ChatRole, message));
+
+        Response<ChatCompletions> response =
+            await _client.GetChatCompletionsAsync(
+                "gpt-35-turbo",
+                _options);
+
+        ChatCompletions completions  = response.Value;
+        string fullresponse = completions.Choices[0].Message.Content;
+        _options.Messages.Add(completions.Choices[0].Message);
+
+        return fullresponse;
+    }
+
+    public Task<string> AskAsync(string message, IChatMessageParameters parameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> AskMultipleAsync(string message, int countOfAnswers)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> AskMultipleAsync(string message, int countOfAnswers, IChatMessageParameters parameters)
+    {
+        throw new NotImplementedException();
+    }
+}
